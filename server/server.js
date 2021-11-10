ec2fe431a193170889e61e0df50bd7eba0a2d50a
Sigurd Grøneng <sigurdgroneng@gmail.com>
2021-11-10 08:51:51 +0100

    Add local auth via wonderwall
    Co-authored-by: sigurdgroneng <sigurdgroneng@nav.no>
    Co-authored-by: rfc3092 <cato.olsen@nav.no>

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '../build')));
+// Redirect to local
+app.listen(port);
 
-app.listen(port)
-
-module.exports = {}
+module.exports = {};
