9cf148ebe0d961f7b2d353b2796dd787f0e34ca0
sigurdgroneng <sigurdgroneng@nav.no>
2021-11-16 10:55:32 +0100

    Style table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -10,19 +10,40 @@ interface Props<T> {
   columns: Column<keyof T>[];
 }
 
+const BehandlingsTag = () => {
+  return (
+    <div className="h-20 w-20 flex justify-center border border-purple-200 bg-purple-100 rounded text-xs font-bold">
+      F
+    </div>
+  );
+};
+
 const Table = <T extends {}>({ data, columns }: Props<T>) => {
   return (
-    <table>
-      <tr>
-        {columns.map((column) => (
-          <th>{column.name}</th>
-        ))}
-      </tr>
+    <table className="ml-40">
+      <thead>
+        <tr>
+          {columns.map((column, index) => (
+            <th key={index} className="first:pl-16 text-left pr-40 h-32">
+              {column.name}
+            </th>
+          ))}
+        </tr>
+      </thead>
       <tbody>
-        {data.map((row) => (
-          <tr>
-            {columns.map((column) => (
-              <td>{row[column.key]}</td>
+        {data.map((row, index) => (
+          <tr
+            key={index}
+            className="text-left h-32 odd:bg-gray-100 border-t-2 last:border-b-2 border-gray-200"
+          >
+            {columns.map((column, index) => (
+              <td key={index} className="first:pl-16 pr-40">
+                {column.key === 'treatmentType' ? (
+                  <BehandlingsTag />
+                ) : (
+                  row[column.key]
+                )}
+              </td>
             ))}
           </tr>
         ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,31 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      'gray-100': '#F8F8F8',
+      'gray-200': '#C6C2BF',
+      'purple-100': '#E0DAE7',
+      'purple-200': '#5C4378',
+    },
+    fontSize: {
+      xs: '14px',
+    },
+    extend: {
+      spacing: {
+        16: '16px', // S
+        20: '20px', // M
+        24: '24px', // M+
+        32: '32px', // L
+        40: '40px', // XL
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      padding: ['first'],
+      backgroundColor: ['odd'],
+      borderWidth: ['last'],
+    },
   },
   plugins: [],
-}
+};
