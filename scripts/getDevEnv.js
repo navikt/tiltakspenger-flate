14906b664e8c158a86458363f0c09aa311b8521e
sigurdgroneng <sigurdgroneng@nav.no>
2021-11-30 13:14:02 +0100

    Add script for getting .env

---
+++ scripts/getDevEnv.js
@@ -0,0 +1,13 @@
+const { execSync } = require('child_process');
+
+const podType = 'tpts-tiltakspenger-flate';
+const podname = execSync('kubectl get pods', { encoding: 'UTF-8' })
+  .split('\n')
+  .find((line) => line.startsWith(podType))
+  .split(' ')[0];
+console.log('Using pod: ', podname);
+const result = execSync(
+  `kubectl exec --stdin --tty --namespace tpts ${podname} -c tpts-tiltakspenger-flate -- printenv | egrep "AZURE_APP_(WELL_KNOWN|CLIENT_ID|JWK=)" > .env`,
+  { encoding: 'UTF-8' }
+);
+console.log(result);
