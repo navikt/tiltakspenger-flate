a3d231857c0f516d616ef3ffb6eac2859a2c2500
sigurdgroneng <sigurdgroneng@nav.no>
2021-12-16 13:21:34 +0100

    Finish getEnv script

--- scripts/getDevEnv.js
+++ scripts/getDevEnv.js
@@ -1,3 +1,4 @@
+const filesSystem = require('fs');
 const { execSync } = require('child_process');
 
 const podType = 'tpts-tiltakspenger-flate';
@@ -7,7 +8,9 @@ const podname = execSync('kubectl get pods', { encoding: 'UTF-8' })
   .split(' ')[0];
 console.log('Using pod: ', podname);
 const result = execSync(
-  `kubectl exec --stdin --tty --namespace tpts ${podname} -c tpts-tiltakspenger-flate -- printenv | egrep "AZURE_APP_(WELL_KNOWN|CLIENT_ID|JWK=)" > .env`,
+  `kubectl exec --stdin --tty --namespace tpts ${podname} -c tpts-tiltakspenger-flate -- printenv | egrep "AZURE_APP_(WELL_KNOWN|CLIENT_ID|JWK=)"`,
   { encoding: 'UTF-8' }
 );
-console.log(result);
+console.log('Env fetched');
+filesSystem.writeFileSync('.env', result);
+console.log('File written');
