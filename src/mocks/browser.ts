85c70d0eca063e80767072f9e4503b28c46e0304
sigurdgroneng <sigurdgroneng@nav.no>
2022-01-17 16:11:12 +0100

    Set base path for service-worker

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,7 +1,12 @@
 import { setupWorker } from 'msw';
 import { getMockHandlers } from './handlers';
+import { basePath } from '../routes';
 
 export const mockHttp = () => {
   const worker = setupWorker(...getMockHandlers());
-  return worker.start();
+  return worker.start({
+    serviceWorker: {
+      url: basePath + 'mockServiceWorker.js',
+    },
+  });
 };
