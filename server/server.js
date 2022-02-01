dfdb3bef837d4d0c71e5f99b7a8034656966d78d
sigurdgroneng <sigurdgroneng@nav.no>
2022-02-01 14:45:11 +0100

    Proxy /api -> backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../build')));
+app.use(
+  '/api/*',
+  createProxyMiddleware({
+    target: 'https://tpts-tiltakspenger-mottak.dev.intern.nav.no/',
+  })
+);
 // Redirect to local
 app.listen(port);
 
