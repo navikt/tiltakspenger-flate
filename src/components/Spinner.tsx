6d4a6b87206b5f761a8c73085157d0faf4a42b3f
sigurdgroneng <sigurdgroneng@nav.no>
2022-02-03 13:56:20 +0100

    Fix types

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, ReactElement } from 'react';
 
 const Spinner: FC<{ isLoading: boolean }> = ({ isLoading, children }) => {
-  return isLoading ? <div>Loading ...</div> : children;
+  return isLoading ? <div>Loading ...</div> : (children as ReactElement);
 };
 
 export default Spinner;
