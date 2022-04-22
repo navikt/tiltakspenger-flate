24e4c0aa523661c37af971c1d9e06c4f15546c30
sigurdgroneng <sigurdgroneng@nav.no>
2022-04-22 15:40:49 +0200

    Remove ununecessary type override after DS-update

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,17 +1,3 @@
 interface ImportMeta {
   env: { MODE: string; BASE_URL: string };
 }
-
-declare module '@navikt/ds-react-internal' {
-  import { FC, ReactNode } from 'react';
-  import {
-    Dropdown as RealDropdown,
-    Header as _Header,
-  } from '@navikt/ds-react-internal';
-  type FixedDropdownType = FC<{ children: ReactNode }> & {
-    Toggle: ToggleType;
-    Menu: MenuType;
-  };
-  export const Dropdown: FixedDropdownType = RealDropdown;
-  export const Header = _Header;
-}
