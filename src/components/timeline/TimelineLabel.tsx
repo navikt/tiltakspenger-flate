b4227895f45f6d0cd47a320060c38c454a2066c0
sigurdgroneng <sigurdgroneng@nav.no>
2022-05-25 16:02:16 +0200

    Add more width fore timeline-labels

--- src/components/timeline/TimelineLabel.tsx
+++ src/components/timeline/TimelineLabel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TimelineLabel = ({ label }: { label: string }) => {
   return (
-    <span className="font-bold w-36 mr-2 border-r self-stretch py-2">
+    <span className="font-bold w-60 mr-2 border-r self-stretch py-2">
       {label}
     </span>
   );
