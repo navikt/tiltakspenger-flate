cb2832b3c9e70ccba416b1441ba43a802b7364df
sigurdgroneng <sigurdgroneng@nav.no>
2022-06-27 10:21:31 +0200

    Add person recoil-state

--- src/state/person.ts
+++ src/state/person.ts
@@ -6,8 +6,28 @@ export const personState = atom<PersonDTO | undefined>({
   default: undefined,
 });
 
+export const behandlingsState = selector({
+  key: 'behandling',
+  get: ({ get }) => get(personState)?.behandlinger[0],
+});
+
 // TODO: Use selected behandling, not first
 export const vilkarsKategoriState = selector({
   key: 'vilkaar',
-  get: ({ get }) => get(personState)?.behandlinger[0]?.vurderinger || [],
+  get: ({ get }) => get(behandlingsState)?.vurderinger || [],
+});
+
+export const barnState = selector({
+  key: 'barn',
+  get: ({ get }) => get(personState)?.personalia?.barn || [],
+});
+
+export const tiltakState = selector({
+  key: 'tiltak',
+  get: ({ get }) => get(behandlingsState)?.tiltak,
+});
+
+export const soknadState = selector({
+  key: 'soknad',
+  get: ({ get }) => get(behandlingsState)?.sknad,
 });
