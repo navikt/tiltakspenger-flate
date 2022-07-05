b4be3ec46087db01b61fb4d9455abcac57b34db0
sigurdgroneng <sigurdgroneng@nav.no>
2022-07-05 10:14:20 +0200

    Remove unused routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
-import ApplicationListPage from './features/soknadList/ApplicationListPage';
-import PersonPage from './features/person/PersonPage';
-import PaymentsTable from './features/person/PaymentsTable';
-import TestPage from './features/TestPage';
 import React from 'react';
-import PersonInfo from './features/person/PersonInfo';
-import EmptyPage from './features/EmptyPage';
-import BehandlingsPage from './features/behandling/BehandlingsPage';
 
 export const basePath = process.env.NEXT_PUBLIC_BASE_URL || '/';
 
@@ -18,32 +10,3 @@ export const personPath = ({
   fnr: string;
   soknadId: string;
 }) => `${basePath}person/${fnr}/soknad/${soknadId}`;
-
-export const paths = {
-  ApplicationPage: `${basePath}`,
-  PersonPage: `${basePath}person/:fnr/*`,
-  PaymentsTable: 'payment/:paymentId',
-  Content: 'soknad/:soknadId',
-  PersonFallback: '*',
-  Behandling: `${basePath}/behandling`,
-  Empty: '',
-  Test: '/test',
-};
-
-export const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path={paths.ApplicationPage} element={<ApplicationListPage />} />
-      <Route path={paths.PersonPage} element={<PersonPage />}>
-        <Route path={paths.PaymentsTable} element={<PaymentsTable />} />
-        <Route path={paths.Content} element={<PersonInfo />} />
-        <Route path={paths.PersonFallback} element={<PersonInfo />} />
-      </Route>
-      <Route path={paths.Behandling} element={<BehandlingsPage />} />
-      <Route path={paths.Empty} element={<EmptyPage />} />
-      <Route path={paths.Test} element={<TestPage />} />
-    </Routes>
-  );
-};
-
-export default AppRoutes;
