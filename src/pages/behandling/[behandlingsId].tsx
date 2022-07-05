56f32da7a098828f74a7652aae39077b551e7349
sigurdgroneng <sigurdgroneng@nav.no>
2022-07-05 13:40:00 +0200

    Remove unused imports

--- src/pages/behandling/[behandlingsId].tsx
+++ src/pages/behandling/[behandlingsId].tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import BehandlingsPage from '../../features/behandling/BehandlingsPage';
 
 export default BehandlingsPage;
--- src/pages/person/[...slug].tsx
+++ src/pages/person/[...slug].tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PersonPage from '../../features/person/PersonPage';
 
 export default PersonPage;
