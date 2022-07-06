97a3df39e6112665a2c362b796c5d31b1b61d848
sigurdgroneng <sigurdgroneng@nav.no>
2022-07-06 09:23:45 +0200

    Make sure msw is setup before first request

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,15 +1,17 @@
+import logger from "../server/logger";
+
 if (typeof window === 'undefined') {
   import("./server")
     .then(({ server }) => {
+      logger.info("Mocking enabled")
       server.listen();
     })
 } else {
+  // Changing import syntac to import().then(...) will delay mocking-setup til
+  // after onMount-requests are fired and they will not be handled by msw
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  import("./browser")
-    .then(({ mockHttp }) => {
-      mockHttp();
-    })
-
+  const { mockHttp } = require("./browser")
+  mockHttp();
 }
 
 export {}
