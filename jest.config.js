b4fbaa636efa6f369dedfe0bc8b6e07bf92e8867
sigurdgroneng <sigurdgroneng@nav.no>
2022-07-07 12:10:19 +0200

    Use jest-next config like intended

--- jest.config.js
+++ jest.config.js
@@ -10,24 +10,20 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFiles: ['./scripts/setupTestEnv.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleDirectories: [
+    'node_modules',
+    'src/testUtils',
+    'generated',
+    '<rootDir>/',
+  ],
+  resolver: `${__dirname}/jestResolvers/uuidFixResolver.js`,
+  moduleNameMapper: {
+    generated: `${__dirname}/generated`,
+  },
   testEnvironment: 'jest-environment-jsdom',
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)().then((config) => {
-  return {
-    ...config,
-    // An array of directory names to be searched recursively up from the requiring module's location
-    moduleDirectories: ['node_modules', 'src/testUtils', 'generated'],
-    // A path to a custom resolver
-    resolver: `${__dirname}/jestResolvers/uuidFixResolver.js`,
-    moduleNameMapper: {
-      ...config.moduleNameMapper,
-      generated: `${__dirname}/generated`,
-    },
-    // The paths to modules that run some code to configure or set up the testing environment before each test
-    setupFiles: ['./scripts/setupTestEnv.js'],
-  };
-});
+module.exports = createJestConfig(customJestConfig);
