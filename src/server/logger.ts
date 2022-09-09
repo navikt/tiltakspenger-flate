71b28d263d012a74df9793e9dcc9c25483f56cfc
Hågen Hasle <haagenhasle@gmail.com>
2022-09-09 15:05:09 +0200

    Lagt på convertReqRes true

--- src/server/logger.ts
+++ src/server/logger.ts
@@ -1,4 +1,4 @@
 import pino from 'pino';
 import ecsFormat from '@elastic/ecs-pino-format';
-const logger = pino(ecsFormat());
+const logger = pino(ecsFormat({convertReqRes: true}));
 export default logger;
